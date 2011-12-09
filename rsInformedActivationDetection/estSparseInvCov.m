61e4a803f79ec5cd46d2f5dcef9fb5ee266b33ae
Bernard Ng <bernardyng@gmail.com>
2011-12-09 16:47:55 +0100

    parallel stuff

--- rsInformedActivationDetection/estSparseInvCov.m
+++ rsInformedActivationDetection/estSparseInvCov.m
@@ -9,21 +9,24 @@ nSubs = 65;
 for i = 1:nSubs
     sublist{i} = fgetl(fid);
 end
-nSubs = 1; % for testing
+% nSubs = 1; % for testing
 
 % Parameters
 dataType = 1; % 1 = rest, 2 = task
 nGridPts = 5; % Number of grid points for lambda, need to be odd number
-nLevels = 5; % Number of refinements on lambda grid
+nLevels = 6; % Number of refinements on lambda grid
+nROIs = 850;
+K = zeros(nROIs,nROIs,nSubs);
+matlabpool;
 parfor sub = 1:nSubs
     if dataType == 1 % Load rest data
-        load([localpath,'data/imagen/',sublist{sub},'/tcRestParcel1000']);
+        temp = load([localpath,'data/imagen/',sublist{sub},'/tcRestParcel1000']);
         X = loadRegressorsRest(sublist{sub}); 
-        Y = tcRest;
+        Y = temp.tcRest;
     elseif dataType == 2 % Load task data
-        load([localpath,'data/imagen/',sublist{sub},'/tcTaskParcel1000']);
+        temp = load([localpath,'data/imagen/',sublist{sub},'/tcTaskParcel1000']);
         X = loadRegressorsTask(sublist{sub}); 
-        Y = tcTask;
+        Y = temp.tcTask;
     end
     % Normalizing the time courses
     Y = Y-ones(size(Y,1),1)*mean(Y);
@@ -32,11 +35,12 @@ parfor sub = 1:nSubs
     optMethod = 1; % 1 = GL via two-metric projection, 2 = (Friedman,2007)
     kFolds = 2;
     model = 1;
-    K = sparseGGM(Y,paramSelMethod,optMethod,'linear',kFolds,nLevels,nGridPts);
+    K(:,:,sub) = sparseGGM(Y,paramSelMethod,optMethod,'linear',kFolds,nLevels,nGridPts);
 %     modelEvid = @(V)modelEvidence(X,Y,V,model);
 %     K = sparseGGM(Y,paramSelMethod,optMethod,'linear',kFolds,nLevels,nGridPts,modelEvid);
 end
 
-    
-    
-
+for sub = 1:nSubs
+    Krest = K(:,:,sub);
+    save([localpath,'data/imagen/',sublist{sub},'/KrestParcel1000cvProj'],'Krest');
+end
